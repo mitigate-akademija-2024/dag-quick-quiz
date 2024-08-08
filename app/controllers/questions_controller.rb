61abc9c02aaccd39d8ace7f7d4168202c2bbed17
Darta <80467516+gdarta@users.noreply.github.com>
2024-08-08 14:00:35 +0000

    Added question form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,32 @@
 class QuestionsController < ApplicationController
+  before_action :set_quiz, only: [:new, :create]
+
   def index
   end
 
   def create
+    @question = @quiz.questions.new(question_attributes)
+    if @question.save
+      flash.notice = "Question was created."
+      redirect_to quiz_url(@quiz)
+    end
   end
 
   def start
   end
 
   def test
+  end
+
+  def new
+    @question = @quiz.questions.new
+  end
+
+  def set_quiz
+    @quiz = Quiz.find(params[:quiz_id])
+  end
+
+  def question_attributes
+    params.require(:question).permit(:question_text)
   end
 end
